3891a53b6f36e8b792ddac1cc3b7724e2f32ba6a
Sonu Kumar Kushwaha <flyingsonu736@gmail.com>
2025-01-13 03:27:39 +0530

    Delete links.sh

--- links.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo "Content-Type: text/plain"
-echo ""
-echo "Sonu's Favorite Links:"
-echo "1. Personal Website: https://sonukushwaha.com"
-echo "2. GitHub: https://github.com/sonukumarkushwaha"
-echo "3. LinkedIn: https://linkedin.com/in/sonukumarkushwaha"
